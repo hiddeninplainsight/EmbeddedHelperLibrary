3c6879ed40c4094801804d64540813a5d66ef751
Byran Wills-Heath <byran@adgico.co.uk>
2019-07-20 17:03:40 +0100

    Refactor isr_circular_buffer methods to out of class

--- src/ehl/isr_circular_buffer.h
+++ src/ehl/isr_circular_buffer.h
@@ -28,37 +28,44 @@ namespace ehl
 			}
 		};
 
-		size_t length() const
-		{
-			if(tail > head)
-				return (buffer_elements - tail) + head;
-			return head - tail;
-		}
-
-		bool push(T value)
-		{
-			size_t next_head = (head + 1) % buffer_elements;
-
-			if(next_head == tail)
-				return false;
-
-			buffer[head] = value;
-			head = next_head;
-			return true;
-		}
-
-		popped_value pop()
-		{
-			if(tail == head)
-			{
-				return popped_value{};
-			}
-
-			popped_value v{ true, buffer[tail] };
-			tail = (tail + 1) % buffer_elements;
-			return v;
-		}
+		size_t length() const;
+		bool push(T value);
+		popped_value pop();
 	};
+
+	template<typename T, size_t buffer_elements>
+	size_t isr_circular_buffer<T, buffer_elements>::length() const
+	{
+		if(tail > head)
+			return (buffer_elements - tail) + head;
+		return head - tail;
+	}
+
+	template<typename T, size_t buffer_elements>
+	bool isr_circular_buffer<T, buffer_elements>::push(T value)
+	{
+		size_t next_head = (head + 1) % buffer_elements;
+
+		if(next_head == tail)
+			return false;
+
+		buffer[head] = value;
+		head = next_head;
+		return true;
+	}
+
+	template<typename T, size_t buffer_elements>
+	auto isr_circular_buffer<T, buffer_elements>::pop() -> popped_value
+	{
+		if(tail == head)
+		{
+			return popped_value{};
+		}
+
+		popped_value v{ true, buffer[tail] };
+		tail = (tail + 1) % buffer_elements;
+		return v;
+	}
 }
 
 #endif //EMBEDDEDHELPERLIBRARY_CIRCULAR_BUFFER_H
