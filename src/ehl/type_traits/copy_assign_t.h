fd29438e20271b7c2370672d30a716ef494ce687
Byran Wills-Heath <byran@adgico.co.uk>
2025-08-01 11:01:48 +0100

    Fix copy_assign_t to work with clang which doesn't have declval in C++11

--- src/ehl/type_traits/copy_assign_t.h
+++ src/ehl/type_traits/copy_assign_t.h
@@ -1,13 +1,13 @@
 #ifndef EMBEDDEDHELPERLIBRARY_TYPE_TRAITS_COPY_ASSIGN_T_H
 #define EMBEDDEDHELPERLIBRARY_TYPE_TRAITS_COPY_ASSIGN_T_H
 
-#include <utility>
+#include "declval.h"
 
 namespace ehl
 {
 	template <class T>
 	using copy_assign_t =
-		decltype(std::declval<T&>() = std::declval<T const&>());
+		decltype(ehl::declval<T&>() = ehl::declval<T const&>());
 }
 
 #endif  // EMBEDDEDHELPERLIBRARY_TYPE_TRAITS_COPY_ASSIGN_T_H
